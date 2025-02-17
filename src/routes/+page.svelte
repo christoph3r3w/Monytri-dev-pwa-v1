6897d77f1a3f9a63e75f1d501992a59330c8de38
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-02-17 23:22:00 +0100

    general updates
    added
    - generic image
    - document title
    
    updated
    - global css
    - packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,14 @@
-<h1>PWA with service worker</h1>
+<script>
+	import { onMount } from 'svelte';
+	import {PTest} from '$lib'
+</script>
 
-<p>needs to update or show that it updated</p>
-<br>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est velit doloribus commodi illo id, aut, nihil recusandae culpa iure debitis magni! Earum perspiciatis error magnam, ab minus quas quisquam, aliquid nesciunt, cupiditate tenetur sed quaerat aspernatur accusamus ullam veritatis. Ut perspiciatis asperiores mollitia voluptates voluptatum beatae iure dolorem velit natus?</p>
-<p>
-	123
-</p>
+
+<div>
+	<PTest/>
+	
+</div>
 
 <style>
 
-h1{
-	font-size: xx-large;
-	margin-bottom: 1rem;
-}
-
-p{
-	background-color: rgba(255, 255, 0, 0.399);
-}
-	
 </style>
-
-
