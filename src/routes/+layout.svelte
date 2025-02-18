d6d95826701a59dab0b83dd1938caa18fcb0714f
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-02-19 00:42:32 +0100

    layout update #7 #4
    - adjusted styling
    - refactored code
    - footer has a grid structure
    - main has a subgrid structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -60,41 +60,93 @@
 </section>
 
 
-<style>
+<style >
+
 	:root{
 		--mobile:0;
+		--body-padding: 3%;
+
 		@property --mobile{
 			syntax: "<number>"; 
 			initial-value: 0;
 			inherits: true;
     	}
 
-	.body-container{
+	}
+
+
+	:global(.body-container){
 		display: grid;
-		grid-template-columns: auto [content-start] repeat(12,1fr) [content-end] auto;
-		grid-template-rows: [header-start] .15fr [header-end main-start] 2fr [main-end footer-start] 35vh [footer-end];
+		grid-template-columns: var(--body-padding) [content-start] repeat(12,1fr) [content-end] var(--body-padding);
+		grid-template-rows: [header-start] 9dvh [header-end main-start] 2fr [main-end footer-start] 35dvh [footer-end];
 		min-height: 100dvh;
 	}
 
+	/* .body-container > :nth-child(n) > :first-child{
+		container-type: inline-size;
+		outline: solid yellow 1px;
+	} */
+
 	header{
-		background-color: color-mix(in hsl,var(--general-background-color),rgb(188, 45, 45) 50%);
-		grid-row: header-start/header-end;
+		background-color: var(--general-background-color);
+		grid-row: header;
 		grid-column: 1/-1;
+
+		/* &:first-child{container-name:header;} */
+
 		@container style(--mobile:1){
-			height: clamp(50px, 10vh, 10vh);
+			background-color: var(--primary-green-500);
+			height: clamp(50px, 10dvh, 10dvh);
+			position: fixed;
+			top: 0;
+			inset-inline: 0;
 		}
 	}
+
 	main {
 		background-color: var(--general-background-color);
-		display: inline-block;
-		height: 100%;
-		grid-row: main-start/main-end;
+		display: grid;
+		grid-template-columns: subgrid;
+		min-height: 100dvh;
+		grid-row: main;
+		grid-column: 1/-1;
+		
+		container-name: main;
+
+		> :nth-child(n){
+			grid-column: content;
+		}
+
+		@container style(--mobile:1){
+			min-height: unset;
+			height: 300dvh;
+			overflow-y: scroll;
+			overflow-x: clip;
+		}
+		
 	}
 
 	footer{
 		background-color: color-mix(in hsl,var(--general-background-color),rgba(67, 15, 15, 0.642) 80%);
-		grid-row: footer-start/footer-end;
+		grid-row: footer;
 		grid-column: 1/-1;
+		display: grid;
+		grid-template-columns: subgrid;
+		
+		container-name:footer;
+		
+		@container style(--mobile:1){
+			background-color: var(--primary-green-500);
+			grid-template-columns: var(--body-padding) [content-start] repeat(6,1fr) [content-end] var(--body-padding);
+			grid-template-rows: 1fr;
+
+			position: fixed;
+			bottom: 0;
+			inset-inline: 0;
+			height:clamp(50px, 10dvh, 10dvh);
+		}
+	}
+
 	@media  (width < 900px) and (orientation: portrait), (height < 800px) and (orientation: landscape) {
 		:root{
 			--mobile:1;
@@ -104,21 +156,8 @@
 		.body-container{
 			/*chris - greate a grid that would move */
 			grid-template-columns: var(--body-padding) [content-start] repeat(6,1fr) [content-end] var(--body-padding);
-			grid-template-rows: [header-start] 1fr [header-end main-start] 4fr [main-end footer-start] 1fr [footer-end];
+			grid-template-rows: [header-start] clamp(50px, 10vh, 10vh) [header-end main-start] 4fr [main-end footer-start] clamp(50px, 10vh, 10vh) [footer-end];
 			overflow: hidden;
-		}
-
-		main{
-			min-height: unset;
-			height: auto;
-			overflow-y: scroll;
-			overflow-x: clip;
-			
-		}
-
-		footer{
-			outline: solid 10px red;
-			background-color: var(--primary-green-500);
 		}
 	}
 </style>
