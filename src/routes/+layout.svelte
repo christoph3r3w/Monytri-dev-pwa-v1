0dcbfca2d9ec3f3d431153e0bfa05d4a0f114da9
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2025-03-09 17:52:18 +0100

    testing no view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -123,16 +123,16 @@
 		};
 	});
 
-	onNavigate((navigation) => {
-        if(!document.startViewTransition){return};
+	// onNavigate((navigation) => {
+    //     if(!document.startViewTransition){return};
 
-        return new Promise((resolve) =>{
-            document.startViewTransition(async ()=>{
-                resolve();
-                await navigation.complete;
-            })
-        })
-    })
+    //     return new Promise((resolve) =>{
+    //         document.startViewTransition(async ()=>{
+    //             resolve();
+    //             await navigation.complete;
+    //         })
+    //     })
+    // })
 
 	afterNavigate(() => {
 		currentPage();
